0b60a04e818c18a807b72e2bacb6eecc55f8eea3
Geoffrey Arner <geoffarner5@gmail.com>
2022-08-02 22:35:01 -0500

    Made Place-vue but didn't finish

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <nav>
     <router-link to="/">Home</router-link>
     |
-    <router-link to="/about">About</router-link>
+    <router-link to="/places/new.json">All Places</router-link>
+    |
+    <router-link to="/places.json">All Places</router-link>
   </nav>
   <router-view />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,72 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "That Vue-do You Do So Well",
+      places: [],
+      newPlaceParams: {},
+      currentPlace: {},
+      editPlaceParams: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("/places.json").then((response) => {
+        this.places = response.data;
+        console.log("All Places:", this.places);
+      });
+    },
+    createPlace: function () {
+      axios
+        .post("/places.json", this.newPlacesParams)
+        .then((response) => {
+          console.log("New Places.", response.data);
+          this.places.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.quereySelector("/#place-details").showModal();
+    },
+    updatePlaces: function (place) {
+      axios.patch("/places/" + place.id + ".json", this.editPlaceParams).then((response) => {
+        console.log("Place Updated", response.data);
+      });
+    },
+    destroyPlace: function (place) {
+      axios.delete("/places/" + place.id).then((response) => {
+        console.log("Place Deleted.", response.data);
+        var index = this.places.indexOf(place);
+        this.places.splice(index, 1);
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newPlaceParams.name" />
+      Address:
+      <input type="text" v-model="newPlaceParams.address" />
+      <button v-on:click="createPlace()">Make a New Place</button>
+    </div>
+    <div v-for="place in places" v-bind:key="place.id">
+      <p>{{ place.name }}</p>
+      <p>{{ place.address }}</p>
+      <button v-on:click="showPlace(place)">Show Place</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
